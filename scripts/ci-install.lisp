01d60f82174261b5950ba01ad8cd0dd1478b982f
Nikolai Matiushev <egao1980@gmail.com>
2026-08-22 22:12:31 +0100

    CI: match datetime-protocol / cl-stack-tzdata install and test scripts

--- scripts/ci-install.lisp
+++ scripts/ci-install.lisp
@@ -26,7 +26,9 @@
  (lambda ()
    (cl-repo:ensure-system-dependencies "cl-stack-calendars"
      :also-tests t
-     :with '("datetime-protocol/calendars" "cl-stack-tzdata")
+     :sources '(("rove" :ql)))
+   (cl-repo:ensure-system-dependencies "datetime-protocol/calendars"
+     :also-tests t
      :sources '(("rove" :ql)))))
 
 (format t "~&; ci: install phase done~%")
